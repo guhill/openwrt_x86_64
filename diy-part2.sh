7fdeccf05c546813269c501b89354c47f80e376a
guhill <guhill@msn.com>
2021-12-20 02:14:04 +0800

    aaa

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/%R/%R compiled guhill/g' package/base-files/files/etc/openwrt_release
 
+# Modify default version
+sed -i 's/%R/%R compiled guhill/g' package/base-files/files/etc/openwrt_release
